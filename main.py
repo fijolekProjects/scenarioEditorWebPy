abe47d89d5f3a9d013430aecd7e7bb5562fa21cb
mihas <mihas@mihas-R580-R590.(none)>
2013-04-29 17:38:20 +0200

    stablicowanie danych po submicie

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ urls = (
 
 render = web.template.render('templates/', base='base')
 
+
+
 app = web.application(urls, globals())
 
 
@@ -17,13 +19,35 @@ app = web.application(urls, globals())
 class Index:
     def GET(self):
         return render.questForm("Quest Form")
+    
     def POST(self):
-        inputFromForm = web.input()
-        return inputFromForm
+        form = web.data()
+        print 'input oryginalny'
+        print form
         
-    
-    
+        print ''
         
+        print 'input po usunieciu znaku &'
+        formList1 = form.split('&')
+        print formList1
+        
+        resultList =[]
+        for i in formList1:
+            pair = i.split('=')
+            if (pair[1] != "" and pair[1] != "No"):
+                resultList.append(pair)
+        print ''
+        print 'input po zmagazynowaniu w liste ktorej elementami sa dwuelementowe listy'
+        
+        print resultList
+        
+        print ''
+        print 'a tak sie dobrac do poszczegolnych skladowych'
+        
+        for i in xrange(len(resultList)):
+            print resultList[i][0] + '====' +  resultList[i][1]
+        
+        return resultList
 
 if __name__ == '__main__':
     app.run()
