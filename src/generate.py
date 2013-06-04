ba981ad7f3535b4bd8a56a6753ee752b8889198a
Tomek Kubrak <t.kubrak22@gmail.com>
2013-06-04 21:48:36 +0200

    Dodana dokumentacja i zmieniona nazwa folderu ze zrodlami

---
+++ src/generate.py
@@ -0,0 +1,220 @@
+import re
+from lxml import etree
+from iteratorWrapper import iteratorWrapper
+import constants
+
+
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+
+@singleton
+class ScenarioGenerator(object):
+    '''
+    Xml-generating class
+    '''
+    root = etree.Element(constants.rootName) 
+    filename = "scenario.xml"
+    FILE = open(filename, "w")
+    
+    
+    def cutFormList(self,inputList):      
+        '''
+    
+        Cuts whole scenario into single quests
+
+        '''
+
+        questList = []    
+        tempQuestMark = 0;
+                
+        for index,item in enumerate(inputList):
+            if item[0] == constants.endOfStringMark:
+                
+                questList.append(inputList[tempQuestMark:index])
+                tempQuestMark = index+1
+              
+             
+        return questList
+            
+
+        
+    
+  
+    def fillQuest(self, xmlData):
+        '''
+        
+        Fills  single quest data
+        
+        '''
+        eventType = xmlData[0][1]
+        quest = etree.Element(eventType, id = xmlData[1][1])
+        
+       
+        
+        i = iter(xmlData)    
+        
+        item = i.next() #iterate twice in order to skip two first 
+        item = i.next()
+        
+        questWrapper = iteratorWrapper(None,i,item)
+        
+      
+        
+        for questWrapper.item in questWrapper.iterator:         
+  
+            questWrapper.attribute = etree.Element(questWrapper.item[0])           
+            questWrapper.attribute.text = questWrapper.item[1] 
+                     
+            attribute = self.addIndentation(questWrapper)
+    
+            quest.append(attribute)
+        self.root.append(quest)
+        
+        return 0
+  
+
+        
+    def xmlFinish(self):
+        '''
+        Writes parsed data into a .xml file
+        '''
+        tempXml = etree.tostring(self.root, pretty_print=True)
+        self.FILE.writelines(tempXml)
+        
+        return 0
+
+   
+   
+   
+    def addMultiChild(self, prefix, wrapper,section):
+        '''
+        Adds children in a certain section
+        '''
+        if re.match(section,wrapper.item[0]):
+            wrapper.item = wrapper.iterator.next()
+        
+        localRoot = etree.Element(prefix[0:len(prefix)-2])
+        topicTest = 0
+        
+        while re.match(prefix, wrapper.item[0]):
+                            
+                    topicTest = 1
+                    
+                    task = self.createSingleTask(prefix, wrapper) 
+                    
+                    
+                    localRoot.append(task)    
+                          
+                    try:                          
+                        wrapper.item = wrapper.iterator.next()
+                    except StopIteration:
+                        break
+        
+        if topicTest==1:
+            wrapper.attribute.append(localRoot)
+            
+        return wrapper.item
+    
+
+    
+    def addComplexChild(self, indentList, wrapper):
+        '''
+        Adds a child which can have other children
+        '''
+        if re.match(indentList[0], wrapper.item[0]):
+                
+                wrapper.attribute = etree.Element(indentList[0][0:len(indentList[0])-2])
+                wrapper.attribute.text = ''    
+               
+                if not indentList[1]:
+              
+                    wrapper.item = self.addChild(indentList[0], wrapper)
+      
+                        
+                else:
+                      wrapper.item = wrapper.iterator.next()
+                      for k in range (0,len(indentList[1])):
+                       
+                          wrapper.item = (self.addMultiChild(indentList[1][k], wrapper, indentList[0]))
+                          
+            
+        return wrapper.attribute
+    
+    
+    
+    def addIndentation(self, wrapper):
+        '''
+        Adds indentation to main tree in quest
+        '''
+        tempAttribute = wrapper.attribute
+        
+        for k in range(0,len(constants.indentationList)):
+        
+            if re.match(constants.indentationList[k][0], wrapper.item[0]):    
+                          
+                 tempAttribute = None
+                 
+                 tempAttribute = self.addComplexChild(constants.indentationList[k], wrapper)  
+                 break
+ 
+        return tempAttribute
+
+ 
+    def addChild(self, prefix, wrapper):
+         '''
+         Adds children to a tree defined by a prefix
+         '''
+         
+         while re.match(prefix,wrapper.item[0]):
+                           
+            task = self.createSingleTask(prefix, wrapper)  
+                
+            wrapper.attribute.append(task)    
+                         
+            try:                          
+                wrapper.item = wrapper.iterator.next()
+            except StopIteration:
+                break
+        
+                 
+         return wrapper.item    
+     
+       
+    def createSingleTask(self, prefix, wrapper): 
+        '''
+        Returns one task which can be appended to tree in other method
+        '''
+        temp = wrapper.item[0]                  
+        temp = temp[len(prefix)-1:]
+        
+        if re.match(constants.valueSuffix,temp):
+                            
+            temp = temp[:len(temp)-len(constants.valueSuffix)+2]
+            task = etree.Element(temp)
+            task.text = wrapper.item[1]
+            wrapper.item = wrapper.iterator.next()              
+            task.set(wrapper.item[0],wrapper.item[1])
+            
+        else:
+            
+            task = etree.Element(temp)
+            task.text = wrapper.item[1]
+        
+        return task
+    
+    
+    def fillXml(self,questList):
+        '''
+        Fill xml with quest
+        '''
+        for item in questList:
+            self.fillQuest(item)
+            
+            
+            
---
+++ src/iteratorWrapper.py
@@ -0,0 +1,14 @@
+
+
+class iteratorWrapper(object):
+    '''
+    Contains data necessery to iterate list in ScenarioGenerator class
+    '''
+
+    def __init__(self,attribute,iterator,item):
+        
+        self.attribute = attribute
+        self.iterator = iterator
+        self.item = item
+       
+        
